d190b90ffd669ef62c450c09015c5d2f1715e616
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-11-26 13:43:35 +0000

    global variables and onclick events

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-body {
-  background-color: #010165;
-  margin: 0px;
+//global variables
+
+const doorImage1 = document.getElementById('door1') 
+const doorImage2 = document.getElementById('door2') 
+const doorImage3 = document.getElementById('door3') 
+
+const botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg'
+const beachDoorPath = 'https://content.codecademy.com/projects/chore-door/images/beach.svg'
+
+doorImage1.onclick = () => {
+  // alert('you clicked me!')
+  doorImage1.src = botDoorPath
+}
+doorImage2.onclick = () => {
+  // alert('you clicked me!')
+  doorImage2.src = beachDoorPath
+}
+
+doorImage3.onclick = () => {
+  // alert('you clicked me!')
+  doorImage3.src = beachDoorPath
 }
